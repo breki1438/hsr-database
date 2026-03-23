6a22285393d0caefba3e6204eac80fc342356479
Mikołaj Jędo <brekus1438@gmail.com>
2026-03-23 17:34:21 +0100

    main page & sidebar basic

---
+++ src/components/ui/side-bar.tsx
@@ -0,0 +1,7 @@
+export default function SideBar() {
+    return (
+        <div className={"w-12.5 hover:w-50 duration-200 ease-in-out h-screen bg-slate-600 absolute z-1000 drop-shadow-lg"}>
+            ech
+        </div>
+    )
+}
